bb1ca4cb85f320775ef3a0fb688548cc68d5d5ad
Markus Klink <justjoheinz@gmail.com>
2014-03-22 19:25:06 +0100

    Improved logging

--- src/main/scala/oose/sbtjavaapigen/JavaApi.scala
+++ src/main/scala/oose/sbtjavaapigen/JavaApi.scala
@@ -7,24 +7,51 @@ import Keys._
 import complete.DefaultParsers._
 import oose.sbtjavaapigen.generator._
 
+/**
+ * Plugin entry point for the
+ * sbt-javaapi-gen plugin
+ * see: https://github.com/oose/sbt-javaapi-gen
+ */
 object JavaApi extends Plugin {
 
-  lazy val javaapi = taskKey[Seq[File]]("Generates a scala API for java classes")
+  /**
+   * Setting for the generator task.
+   */
+  lazy val javaapi =
+    taskKey[Seq[File]]("Generates a set of implicit classes to access java classes")
 
+  /**
+   * Settings for the set of classes to be transformed to implicit classes.
+   */
   lazy val javaapiclasses = settingKey[Set[String]]("Classes to generate")
-  
+
+  /**
+   * A list of warnings and the result of the generation process.
+   */
+  type Response = (List[String], String)
+
+ 
+
   // TODO output file is hardcoded - fail
-  lazy val javaapiTaskImpl: Initialize[Task[Seq[File]]] =
-   Def.task { 
-      val output = ((sourceManaged in Compile)/ "/scala/api.scala").value
-      val log = streams.value.log
-      log.info("Generating sources...")
-      IO.write(output,Generator(javaapiclasses.value))
-      Seq(output)   
-  }
-  
-  override val settings = Seq( 
-      sourceGenerators in Compile <+= (javaapi in Compile), 
-      javaapiclasses := Set.empty,
-      javaapi := javaapiTaskImpl.value )
+  /**
+   * Task definition.
+   */
+    lazy val javaapiTaskImpl: Initialize[Task[Seq[File]]] =
+      Def.task {
+        val output = ((sourceManaged in Compile) / "/scala/api.scala").value
+        val log = streams.value.log
+        log.info("Generating sources...")
+        val result: Response = Generator(javaapiclasses.value)
+        IO.write(output, result._2)
+        result._1.map(classNotFound => log.warn(classNotFound))
+        Seq(output)
+      } 
+
+  /**
+   * Provide default settings.
+   */
+  override val settings = Seq(
+    sourceGenerators in Compile <+= (javaapi in Compile),
+    javaapiclasses := Set.empty,
+    javaapi <<= javaapiTaskImpl)
 }
