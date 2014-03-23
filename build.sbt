61821cb7559b39eeae9e5843989727c0296020de
Markus Klink <justjoheinz@gmail.com>
2014-03-23 11:37:24 +0100

    added test and test dependencies

--- build.sbt
+++ build.sbt
@@ -5,7 +5,8 @@ organization := "oose"
 version := "0.3-SNAPSHOT"
 
 libraryDependencies ++= Seq(
-  "org.scalaz" %% "scalaz-core" % "7.0.6"
+  "org.scalaz" %% "scalaz-core" % "7.0.6",
+  "org.specs2" %% "specs2" % "2.3.10" % "test"
 )
 
 sbtPlugin := true
---
+++ src/test/scala/oose/sbtjavaapigen/javaapigenspec.scala
@@ -0,0 +1,38 @@
+package oose.sbtjavaapigen
+
+import oose.sbtjavaapigen.generator._
+import org.specs2.mutable._
+
+class  JavaApiGenSpec extends SpecificationWithJUnit {
+
+  def wrapInPackage(packageName: String)(c: String) = (packageName + "." + c)
+
+  val io = Set(
+      "File") map wrapInPackage ("java.io")
+
+  val (warnings, output)  = Generator(Seq.empty,
+    io)
+    
+  "The API Generator" should {
+    "run without warnings" in {
+      warnings must beEmpty
+    }
+    
+    "create output" in {
+      output must not beEmpty
+    }
+    
+    "create the correct package" in {
+      output must contain("package java.io")
+    }
+    
+    "contain an api object" in {
+      output must contain("object api")
+    }
+    
+    "create the correct class" in {
+      output must contain("implicit class ScalaFile")
+    }
+  }
+  
+}
