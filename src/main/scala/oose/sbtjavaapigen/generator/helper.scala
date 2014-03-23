6bca474be8e53760b2c3fb19fe10becd9744e750
Markus Klink <justjoheinz@gmail.com>
2014-03-23 09:22:50 +0100

    added more scala keywords

--- src/main/scala/oose/sbtjavaapigen/generator/helper.scala
+++ src/main/scala/oose/sbtjavaapigen/generator/helper.scala
@@ -2,21 +2,25 @@ package oose.sbtjavaapigen.generator
 
 object Helper {
   import scalaz._
-  
-  /** Define a Writer to store required Imports */
+
+  /**
+   *  Define a Writer to store required Imports
+   */
   type ImportWriter[A] = Writer[List[ClassPackage], A]
 
-  /** Prepend some tabulators as spaces in front of a string. */
+  /**
+   *  Prepend some tabulators as spaces in front of a string.
+   */
   def tab(tabulator: Int): String = List.fill(tabulator * 2)(" ").mkString
-  
+
   implicit class MethodString(m: String) {
-    
+
     /**
      * converts a property name in CamelCase which has been
      * derived from a java function, e.g. getMyFoo() [MyFoo] to
      * myFoo, taking into account that scala has some extra
      * reserved keywords.
-     * For example getType() with is not turned into the lower 
+     * For example getType() is not turned into the lower
      * case version.
      */
     def scalaConvention() =
@@ -25,6 +29,12 @@ object Helper {
       else {
         m match {
           case "Type" => "Type"
+          case "Sealed" => "Sealed"
+          case "Yield" => "Yield"
+          case "Match" => "Match"
+          case "Implicit" => "Implicit"
+          case "Val" => "Val"
+          case "Var" => "Var"
           case _ =>
             val chars = m.toCharArray
             chars(0) = chars(0).toLower
